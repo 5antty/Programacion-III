448a0f39ffa6efdfce00bab587de46e05e33b90f
5antty <santi51423@gmail.com>
2023-05-31 00:35:16 -0300

    metodo devolver con excepciones terminado?

--- src/Practica7/a/Mapa.java
+++ src/Practica7/a/Mapa.java
@@ -18,81 +18,129 @@ public class Mapa {
         this.mapaCiudades = mapaCiudades;
     }
 
-    /*public ListaGenerica<String> devolverCamino(String city1, String city2) {
+    // private ListaGenerica<String> devolverCaminoExc(String city1, String city2) {
+    // ListaGenericaEnlazada<String> res = new ListaGenericaEnlazada<String>();
+    // boolean[] marca = new
+    // boolean[this.getMapaCiudades().listaDeVertices().tamanio()];
+
+    // ListaGenerica<Vertice<String>> lis = new
+    // ListaGenericaEnlazada<Vertice<String>>();
+    // ListaGenerica<Arista<String>> ady = null;
+    // Cola<Vertice<String>> q = new Cola<Vertice<String>>();
+
+    // // implementar el while hasta que el dato del vertice sea la city1
+    // ListaGenerica<Vertice<String>> aux1 =
+    // this.getMapaCiudades().listaDeVertices();
+    // aux1.comenzar();
+    // Vertice<String> aux2 = aux1.proximo();
+    // while (!aux1.fin() && !(aux2.dato().equals(city1))) {
+    // aux2 = aux1.proximo();
+    // }
+
+    // q.encolar(aux2);
+    // marca[aux2.posicion()] = true;
+
+    // while (!q.esVacia()) {
+    // Vertice<String> cityActual = q.desencolar();
+    // // Agrego la ciudad a la lista de ciudades
+    // lis.agregarFinal(cityActual);
+
+    // if (cityActual.dato().equals(city2))
+    // break;
+
+    // ady = this.getMapaCiudades().listaDeAdyacentes(cityActual);
+    // ady.comenzar();
+    // while (!ady.fin()) {
+    // Arista<String> arista = ady.proximo();
+    // int j = arista.verticeDestino().posicion();
+    // if (!marca[j]) {
+    // Vertice<String> w = arista.verticeDestino();
+    // marca[j] = true;
+    // q.encolar(w);
+    // }
+    // }
+    // }
+
+    // lis.comenzar();
+    // Vertice<String> cityAct;
+    // while (!lis.fin()) {
+    // cityAct = lis.proximo();
+    // res.agregarFinal(cityAct.dato());
+    // }
+
+    // // T(n) de un alg de un grafo se anota O(|V|+|E|)
+    // return res;
+    // }
+
+    public ListaGenerica<String> devolverCamino(String city1, String city2) {
         ListaGenericaEnlazada<String> res = new ListaGenericaEnlazada<String>();
         boolean[] marca = new boolean[this.getMapaCiudades().listaDeVertices().tamanio()];
-
-        ListaGenerica<Vertice<String>> lis = new ListaGenericaEnlazada<Vertice<String>>();
-        ListaGenerica<Arista<String>> ady = null;
-        Cola<Vertice<String>> q = new Cola<Vertice<String>>();
-
-        // implementar el while hasta que el dato del vertice sea la city1
         ListaGenerica<Vertice<String>> aux1 = this.getMapaCiudades().listaDeVertices();
         aux1.comenzar();
         Vertice<String> aux2 = aux1.proximo();
         while (!aux1.fin() && !(aux2.dato().equals(city1))) {
             aux2 = aux1.proximo();
         }
-
-        q.encolar(aux2);
-        marca[aux2.posicion()] = true;
-
-        while (!q.esVacia()) {
-            Vertice<String> cityActual = q.desencolar();
-            // Agrego la ciudad a la lista de ciudades
-            lis.agregarFinal(cityActual);
-
-            if (cityActual.dato().equals(city2))
-                break;
-
-            ady = aux.listaDeAdyacentes(cityActual);
-            ady.comenzar();
-            while (!ady.fin()) {
-                Arista<String> arista = ady.proximo();
-                int j = arista.verticeDestino().posicion();
-                if (!marca[j]) {
-                    Vertice<String> w = arista.verticeDestino();
-                    marca[j] = true;
-                    q.encolar(w);
-                }
-            }
-        }
-
-        lis.comenzar();
-        Vertice<String> cityAct;
-        while (!lis.fin()) {
-            cityAct = lis.proximo();
-            res.agregarFinal(cityAct.dato());
-        }
-
-        // T(n) de un alg de un grafo se anota O(|V|+|E|)
+        int i = aux2.posicion();
+        if (i < marca.length)
+            devolverCamino(i, this.getMapaCiudades(), res, marca, city2);
         return res;
-    }*/
-    
-    public ListaGenerica<String> devolverCamino(String city1, String city2){
-    	ListaGenericaEnlazada<String> res = new ListaGenericaEnlazada<String>();
-    	boolean[] marca = new boolean[this.getMapaCiudades().listaDeVertices().tamanio()];
-    	ListaGenerica<Vertice<String>> aux1 = this.getMapaCiudades().listaDeVertices();
+    }
+
+    private boolean devolverCamino(int i, Grafo<String> grafo,
+            ListaGenericaEnlazada<String> lis, boolean[] marca,
+            String city2) {
+        marca[i] = true;
+        Vertice<String> v = grafo.listaDeVertices().elemento(i);
+        if (v.dato().equals(city2)) {
+            lis.agregarFinal(v.dato());
+            return true;
+        }
+        ListaGenerica<Arista<String>> ady = grafo.listaDeAdyacentes(v);
+        ady.comenzar();
+        while (!ady.fin()) {
+            int j = ady.proximo().verticeDestino().posicion();
+            if (!marca[j])
+                if (!this.devolverCamino(j, grafo, lis, marca, city2))
+                    lis.agregarInicio(v.dato());
+        }
+        return false;
+    }
+
+    public ListaGenerica<String> devolverCaminoExceptuando(String city1, String city2, ListaGenerica<String> cities) {
+        ListaGenericaEnlazada<String> res = new ListaGenericaEnlazada<String>();
+        boolean[] marca = new boolean[this.getMapaCiudades().listaDeVertices().tamanio()];
+        ListaGenerica<Vertice<String>> aux1 = this.getMapaCiudades().listaDeVertices();
         aux1.comenzar();
         Vertice<String> aux2 = aux1.proximo();
         while (!aux1.fin() && !(aux2.dato().equals(city1))) {
             aux2 = aux1.proximo();
         }
+        int j = aux2.posicion();
+
+        for (int i = 0; i < cities.tamanio(); i++) {
+            this.dceRecu(j, this.getMapaCiudades(), marca, res, cities.elemento(i));
+        }
         return res;
     }
-    
 
-    public ListaGenerica<String> devolverCaminoExceptuando(String city1, String city2, ListaGenerica<String> cities) {
-        ListaGenerica<String> res = new ListaGenericaEnlazada<String>();
-        ListaGenerica<String> camino = this.devolverCamino(city1, city2);
-
-        for (int i = 0; i < camino.tamanio(); i++) {
-            for (int j = 0; j < cities.tamanio(); j++) {
-                if (!cities.elemento(j).equals(camino.elemento(i)) && !res.incluye(camino.elemento(i))) {
-                    res.agregarFinal(camino.elemento(i));
-                }
+    private boolean dceRecu(int i, Grafo<String> grafo, boolean[] marca, ListaGenericaEnlazada<String> lis,
+            String cities) {
+        marca[i] = true;
+        Vertice<String> v = grafo.listaDeVertices().elemento(i);
+        if (v.dato().equals(cities) && !lis.incluye(v.dato())) {
+            // lis.agregarFinal(v.dato());
+            return true;
+        }
+        ListaGenerica<Arista<String>> ady = grafo.listaDeAdyacentes(v);
+        ady.comenzar();
+        while (!ady.fin()) {
+            int j = ady.proximo().verticeDestino().posicion();
+            if (!marca[j]) {
+                if (!this.dceRecu(j, grafo, marca, lis, cities) && !lis.incluye(v.dato()))
+                    lis.agregarInicio(v.dato());
             }
         }
-        return camino;
+        return false;
     }
 }
--- src/Practica7/a/test/TestMapa.java
+++ src/Practica7/a/test/TestMapa.java
@@ -2,7 +2,6 @@ package Practica7.a.test;
 
 import estructuras.grafos.*;
 import Practica7.a.Mapa;
-import Practica7.a.Recorridos;
 import estructuras.*;
 
 public class TestMapa {
@@ -14,12 +13,14 @@ public class TestMapa {
         Vertice<String> c3 = new VerticeImplListAdy<String>("La Plata");
         Vertice<String> c4 = new VerticeImplListAdy<String>("Lima");
         Vertice<String> c5 = new VerticeImplListAdy<String>("Catamarca");
+        Vertice<String> c6 = new VerticeImplListAdy<String>("Mendoza");
         map.getMapaCiudades().agregarVertice(c);
         map.getMapaCiudades().agregarVertice(c1);
         map.getMapaCiudades().agregarVertice(c2);
         map.getMapaCiudades().agregarVertice(c3);
         map.getMapaCiudades().agregarVertice(c4);
         map.getMapaCiudades().agregarVertice(c5);
+        map.getMapaCiudades().agregarVertice(c6);
         map.getMapaCiudades().conectar(c, c2, 0);
         map.getMapaCiudades().conectar(c2, c3, 0);
         map.getMapaCiudades().conectar(c3, c1, 0);
@@ -28,25 +29,22 @@ public class TestMapa {
         map.getMapaCiudades().conectar(c2, c4, 0);
         map.getMapaCiudades().conectar(c5, c, 0);
         map.getMapaCiudades().conectar(c5, c4, 0);
+        map.getMapaCiudades().conectar(c6, c1, 0);
+        map.getMapaCiudades().conectar(c6, c5, 0);
+        map.getMapaCiudades().conectar(c4, c3, 0);
+
         ListaGenerica<String> res = map.devolverCamino("Caleta", "Comodoro");
         if (res != null)
             System.out.println(res);
-        /*
-         * Recorridos<String> r = new Recorridos<String>();
-         * ListaGenerica<Vertice<String>> lis = r.bfs(map.getMapaCiudades());
-         * while (!lis.fin()) {
-         * System.out.println(lis.proximo().dato());
-         * }
-         * System.out.println();
-         * lis = r.dfs(map.getMapaCiudades());
-         * while (!lis.fin()) {
-         * System.out.println(lis.proximo().dato());
-         * }
-         */
+
         ListaGenerica<String> exc = new ListaGenericaEnlazada<String>();
-        exc.agregarFinal("Caleta");
+        exc.agregarFinal("Comodoro");
         exc.agregarFinal("CABA");
-        res = map.devolverCaminoExceptuando("Catamarca", "CABA", exc);
+
+        res = map.devolverCamino("Mendoza", "La Plata");
+        if (res != null)
+            System.out.println(res);
+        res = map.devolverCaminoExceptuando("Mendoza", "La Plata", exc);
         if (res != null)
             System.out.println(res);
     }
